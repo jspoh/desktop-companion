b5872a2090290c8577945f407af97f191d408158
jspoh <hello@jspoh.dev>
2026-01-31 04:15:56 +0800

    sprite sheet offsets for furniture

--- src/Room/Room.h
+++ src/Room/Room.h
@@ -14,6 +14,198 @@ public:
 
 	int localScale;
 
+	class Furniture {
+	public:
+
+		struct OffsetData {
+			int left, right, width, height;
+		};
+
+		// L -> R, T -> B
+		enum TYPE {
+			WINDOW_1,
+			WINDOW_2,
+			WINDOW_3,
+			WINDOW_4,
+
+			STAND_1,
+			STAND_2,
+			STAND_3,
+
+			BIG_BALL_1,
+			SMALL_BALL_1,
+
+			EXTENDED_STAND_1,
+			EXTENDED_STAND_2,
+			EXTENDED_STAND_3,
+
+			BIG_WINDOW_1,
+			BIG_WINDOW_2,
+			BIG_WINDOW_3,
+			BIG_WINDOW_4,
+
+			SMALL_WINDOW_1,
+			SMALL_WINDOW_2,
+
+			PLANT_1,
+			PLANT_2,
+			PLANT_3,
+			PLANT_4,
+
+			BED_1,
+			BED_2,
+			BED_3,
+			BED_4,
+			BED_5,
+			BED_6,
+
+			PHOTO_1,
+			PHOTO_2,
+			PHOTO_3,
+			PHOTO_4,
+			PHOTO_5,
+			PHOTO_6,
+
+			PLAYHOUSE_1,
+			PLAYHOUSE_2,
+
+			PAINTING_PINEAPPLE,
+
+			WIDE_WINDOW_1,
+			WIDE_WINDOW_2,
+
+			SHELF_1,
+			SHELF_2,
+			SHELF_3,
+			SHELF_4,
+			SHELF_5,
+
+			BIG_PLANT_1,
+			BIG_PLANT_2,
+			BIG_PLANT_3,
+			BIG_PLANT_4,
+
+			D_1,
+			D_2,
+
+			FOOD_1,
+			FOOD_2,
+			FOOD_3,
+			FOOD_4,
+			FOOD_5,
+			FOOD_6,
+			FOOD_7,
+			FOOD_8,
+			FOOD_9,
+			FOOD_10,
+			FOOD_11,
+			FOOD_12,
+
+			WATER_1,
+			WATER_2,
+			WATER_3,
+			WATER_4,
+			WATER_5,
+			WATER_6,
+
+			FOOD_BAG_1,
+			FOOD_BAG_2,
+
+			DD_1,
+			DD_2,
+			DD_3,
+			DD_4,
+			DD_5,
+
+			DICE_1,
+			DICE_2,
+			DICE_3,
+			DICE_4,
+
+			CHEW_1,
+			CHEW_2,
+			CHEW_3,
+			CHEW_4,
+			CHEW_5,
+			CHEW_6,
+
+			BOWTIE_1,
+			BOWTIE_2,
+			BOWTIE_3,
+			BOWTIE_4,
+			BOWTIE_5,
+			BOWTIE_6,
+			BOWTIE_7,
+			BOWTIE_8,
+			BOWTIE_9,
+			BOWTIE_10,
+
+			MOUSE,
+
+			CHAIR_1,
+			CHAIR_2,
+			CHAIR_3,
+			CHAIR_4,
+			CHAIR_5,
+
+			NUM_FURNITURE_TYPES,
+		};
+
+		const std::unordered_map<TYPE, OffsetData> spritesheetOffsets{
+			{WINDOW_1, {0, 0, 95, 95}},
+			{WINDOW_2, {95, 0, 95, 95}},
+			{WINDOW_3, {0, 95, 95, 95}},
+			{WINDOW_4, {95, 95, 95, 95}},
+
+			{STAND_1, 180, 0, 90, 130},
+			{STAND_2, 270, 0, 90, 130},
+			{STAND_3, 360, 0, 90, 130},
+
+			{BIG_BALL_1, {450, 5, 25, 20}},
+			{SMALL_BALL_1, {485, 5, 25, 20}},
+
+			{EXTENDED_STAND_1, {580, 15, 85, 175}},
+
+			{BIG_WINDOW_1, {865, 0, 60, 125}},
+
+			{SMALL_WINDOW_1, {10, 225, 40, 65}},
+
+			{PLANT_1, {130, 190, 25, 30}},
+
+			{BED_1, {200, 135, 110, 85}},
+
+			{PHOTO_1, {450, 200, 30, 45}},
+
+			{PAINTING_PINEAPPLE, {715, 195, 35, 90}},
+
+			{PLAYHOUSE_1, {555, 220, 115, 165}},
+
+			{WIDE_WINDOW_1, {675, 305, 90, 140}},
+
+			{BIG_PLANT_1, {140, 300, 45, 105}},
+
+			{D_1, {200, 435, 55, 75}},
+
+			{FOOD_1, {265, 435, 45, 40}},
+
+			{WATER_1, {395, 415, 45, 40}},
+
+			{FOOD_BAG_1, {525, 550, 35, 50}},
+
+			{DICE_1, {195, 615, 20, 15}},
+
+			{CHEW_1, {200, 685, 45, 35}},
+
+			{BOWTIE_1, {385, 620, 25, 10}},
+
+			{MOUSE, {450, 620, 55, 35}},
+
+			{CHAIR_1, {675, 610, 60, 60}},
+
+
+		}
+	};
+
 private:
 	Room();
 
--- src/entities/Cat.h
+++ src/entities/Cat.h
@@ -356,8 +356,8 @@ public:
 	void setEntityAnimationState(EntityAnimationStates s, std::optional<std::reference_wrapper<TextureManager::JS_SPRITE>> opt_sprite, int animationLoopCount);
 
 	bool isColliding(const sf::Vector2f& p) {
-		if (p.x < pos.x - width / 2.f || p.x > pos.x + width / 2.f) return false;
-		if (p.y < pos.y - height / 2.f || p.y > pos.y + height / 2.f) return false;
+		if (p.x < pos.x - (width * Settings::catScale) / 2.f || p.x > pos.x + (width * Settings::catScale) / 2.f) return false;
+		if (p.y < pos.y - (height * Settings::catScale) / 2.f || p.y > pos.y + (height * Settings::catScale) / 2.f) return false;
 		return true;
 	}
 };
